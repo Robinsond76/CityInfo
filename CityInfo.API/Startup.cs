6ed0a4dd03205dbf672a06efe268887578f51c4f
Robinson Souza <Robinsond76@gmail.com>
2020-10-05 15:33:06 -0400

    Added configuration to serialize our data to be sent as XML if requested by the Accept Header

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace CityInfo.API
 {
@@ -15,7 +17,22 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(); //Note that for ASP.NET Core 3.0+, it's another service
+            services.AddMvc() //Note that for ASP.NET Core 3.0+, it's another service
+                .AddMvcOptions(o =>
+                {
+                    o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter()); // This serializes our data to be sent as XML if requested by the Accept Header
+                });
+                
+                //.AddJsonOptions(o =>
+                //{
+                //    if (o.SerializerSettings.ContractResolver != null)
+                //    {
+                //        var castedResolver = o.SerializerSettings.ContractResolver
+                //            as DefaultContractResolver;
+                //        castedResolver.NamingStrategy = null;
+                //    }
+                //}); This code is for allowing the JSON properties to return capitalized
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
